c27d17e13806f71a6a9f473a4fa74ad72b1ffd62
MohdShahnawaz7465 <shane74570@gmail.com>
2026-08-01 13:36:30 +0000

    Updated Type Conversion examples

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,33 @@
+let score = "alam"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "alam" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
 // ***** Operations ******
 
 let value = 3
