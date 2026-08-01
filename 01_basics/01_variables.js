6676047f21f8fe262f42542224eff3cf1f72e747
MohdShahnawaz7465 <shane74570@gmail.com>
2026-08-01 12:45:27 +0000

    Added variables basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14332;
+let accountEmail = "alam@gmail.com";
+var accountPassword = "12342";
+accountCity = "Bareilly";
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table({accountId,accountEmail,accountPassword,accountCity});
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("alam")
+
+let name="alam"   //string datatype
+let age=20     //number 
+let isLoggedIn=true //boolean
+
+
+console.log(typeof"alam")  
+console.log(typeof undefined) //undefind
+console.log(typeof null); // object
