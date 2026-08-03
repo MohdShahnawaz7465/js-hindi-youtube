8941d75b4dbe2c24946bb9faaace52ebf5c7b9bc
MohdShahnawaz7465 <shane74570@gmail.com>
2026-08-03 13:08:10 +0000

    complets nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,25 @@
+const score=400
+//console.log(score);
+
+const balance =new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const otherNumber=234.5668
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//***********MAth*****//
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(3.22));
+
+console.log(Math.random());
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
