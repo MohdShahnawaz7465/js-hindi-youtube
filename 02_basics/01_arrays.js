98a1cd6684b4d6c2ba4d2003a2fb44b7b704cb08
MohdShahnawaz7465 <shane74570@gmail.com>
2026-08-05 13:40:20 +0000

    complete array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,32 @@
+//array
+
+const myArray=[0,1,2,3,4,5]
+const myheroes=["alam","aman"]
+const myyArray2=new Array(1,3,4,5)
+//console.log(myArray[5]);
+
+//array method 
+//myArray.push(9)
+//myArray.pop()
+
+//myArray.unshift(5)
+//myArray.shift()
+//console.log(myArray.includes(9));
+//console.log(myArray.indexOf(4));
+
+const newArray=myArray.join()
+//console.log(newArray);
+
+//console.log(myArray);
+//slice,splice 
+console.log("A",myArray);
+const myn1=myArray.slice(1,3)
+console.log(myn1);
+console.log("B",myArray);
+
+const myn2=myArray.splice(1,3)
+console.log("c",myArray);
+console.log(myn2);
+
+
+
