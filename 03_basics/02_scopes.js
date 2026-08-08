d7755ec885d92b64c05a9f77ef041dcffd907e2d
MohdShahnawaz7465 <shane74570@gmail.com>
2026-08-08 13:12:34 +0000

    learning scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+// var c = 330
+let a =900
+if(true){
+    let a=19
+    const b=89
+    console.log("INNER: ",a)
+}
+ for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+ }
+console.log(a);
+// console.log(b)
+// console.log(c)
