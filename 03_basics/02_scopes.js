302372c56879d19f96c8eb8a9c471aee4ab2c968
MohdShahnawaz7465 <shane74570@gmail.com>
2026-08-09 13:30:39 +0000

    completed scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,12 +3,43 @@ let a =900
 if(true){
     let a=19
     const b=89
-    console.log("INNER: ",a)
+   // console.log("INNER: ",a)
 }
- for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+ //for (let index = 0; index < array.length; index++)
+  //{
+   // const element = array[index];
     
- }
-console.log(a);
+ //}
+//console.log(a);
 // console.log(b)
 // console.log(c)
+
+
+function one(){
+    const username="alam"
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    two()
+}
+//one()
+if(true){
+    const username="alam"
+    if(username=="alam"){
+        const website="youtube"
+        console.log(username+website);
+        
+    }
+   // console.log(website);
+    
+}
+//console.log(username);
+
+// ******************interesting********
+
+function addone(num){
+    return num+1
+}
+addone(6)
+//console.log();
